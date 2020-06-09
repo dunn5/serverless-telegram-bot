a7d96f2d99d5befe7e6952df5d0e9a18e477fa6d
Andrew Dunn <andrewdunn@Andrews-MacBook-Pro.local>
2020-06-08 23:52:30 -0500

    Only translate if there is text to avoid errors.

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def webhook(request):
         #update = telegram.Update.de_json(request.get_json(force=True), bot)
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        # Reply with the same message
-        result = translator.translate(update.message.text,dest='en').text
-        if (result == update.message.text):
-            result = "English: " + update.message.text + chr(10) + "Russian: " + translator.translate(update.message.text, src='en', dest='ru').text
-        else:
-        	result ="Russian: " + update.message.text + chr(10) + "English: " + result
+        if update.message.text is not None:
+            result = translator.translate(update.message.text,dest='en').text
+            if (result == update.message.text):
+                result = "English: " + update.message.text + chr(10) + "Russian: " + translator.translate(update.message.text, src='en', dest='ru').text
+            else:
+            	result ="Russian: " + update.message.text + chr(10) + "English: " + result
+
         bot.sendMessage(chat_id=chat_id, text=(result), reply_to_message=update.message)
     return "ok"
