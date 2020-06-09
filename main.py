a12a093e641bf0337df3e8f20f67fb323a73de17
Andrew Dunn <andrewdunn@Andrews-MacBook-Pro.local>
2020-06-08 19:22:02 -0500

    Added google translate package and added translation between English and Russian

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 import telegram
+import googletrans
 
 bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
 
+from googletrans import Translator
+translator = Translator()
 
 def webhook(request):
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
         # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+        result = "" + translator.translate(update.message.text, dest='en').text
+        if (result == update.message.text):
+            result = "English: " + update.message.text + chr(10) + "Russian: " + translator.translate(update.message.text, src='en', dest='ru').text
+        else:
+        	result ="Russian: " + update.message.text + chr(10) + "English: " + result
+        bot.sendMessage(chat_id=chat_id, text=(result), reply_to_message=update.message)
     return "ok"
